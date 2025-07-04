9de545443b39daa8425bf50776c06091ceb5d78d
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2025-07-04 23:53:09 +0200

    Update security.py

--- security/security.py
+++ security/security.py
@@ -14,31 +14,28 @@ token_info_context: ContextVar[MyTokenInfo | None] = ContextVar("token_info_cont
 # This is our set of valid API keys. In a real app, you'd check a database.
 class IntrospectionTokenVerifier(TokenVerifier):
     """
-    Ce verifier demande au serveur d'autorisation si un token est valide.
-    Il est complètement agnostique à la manière dont les tokens sont créés.
+    This verifier asks the authorization server if a token is valid. It is completely agnostic to how the tokens are created.
     """
     async def verify_token(self, token: str) -> AccessToken:
-        # Fait un appel HTTP sécurisé à votre endpoint /introspect
+        # call /introceptor to vaidate the token
         async with httpx.AsyncClient() as client:
             try:
                 response = await client.post(
                     f"{AUTHORIZATION_SERVER_URL}/api/auth/introspect",
                     data={"token": token}
                 )
-                response.raise_for_status() # Lève une exception pour les erreurs 4xx/5xx
+                response.raise_for_status()
             except httpx.RequestError as e:
                 print(f"ERROR: Could not connect to introspection endpoint: {e}")
                 return AccessToken(active=False)
 
-        # Crée un objet TokenInfo directement à partir de la réponse JSON
+        # Create the token info object
         token_info_dict = response.json()
         token_info_dict["token"] = token
         token_info_dict["client_id"] = str(token_info_dict.get("user_id"))
         token_info_dict["scopes"] = []
         token_info = MyTokenInfo(**token_info_dict)
 
-        # --- L'ÉTAPE CRUCIALE ---
-        # Définissez la valeur de la variable de contexte pour cette requête.
         token_info_context.set(token_info)
         return MyTokenInfo(**token_info_dict)
     
